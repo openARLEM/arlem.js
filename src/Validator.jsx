6b956b1f6b6ab29eafcf102c7216748ed604f69b
Erik Isaksson <erikis@kth.se>
2021-05-05 09:39:41 +0000

    Build with improved validation presentation

--- src/Validator.jsx
+++ src/Validator.jsx
@@ -9,6 +9,30 @@ import workplaceSchemaRefs from './editor/workplaceSchemaRefs.json';
 import workplaceExample from './editor/workplaceExample.json';
 import { useState } from 'react';
 
+let Ajv = null; // To be assigned when loading JSONEditor
+
+function getActivityValidator() {
+  return Ajv({
+    allErrors: true,
+    verbose: true,
+    jsonPointers: false,
+    $data: true,
+    schemaId: 'auto',
+    schemas: activitySchemaRefs
+  }).compile(activitySchema);
+}
+
+function getWorkplaceValidator() {
+  return Ajv({
+    allErrors: true,
+    verbose: true,
+    jsonPointers: false,
+    $data: true,
+    schemaId: 'auto',
+    schemas: workplaceSchemaRefs
+  }).compile(workplaceSchema);
+}
+
 export default function Validator({ options }) {
   const activityEditorContainerRef = useRef();
   const workplaceEditorContainerRef = useRef();
@@ -26,7 +50,7 @@ export default function Validator({ options }) {
     // Load JSONEditor dynamically at runtime only when it is needed
     import('jsoneditor').then(JSONEditor => {
       JSONEditor = JSONEditor.default;
-      const Ajv = JSONEditor.Ajv;
+      Ajv = JSONEditor.Ajv;
 
       const editorOptions = {
         mode: 'tree',
@@ -42,28 +66,10 @@ export default function Validator({ options }) {
         schema: workplaceSchema,
         schemaRefs: workplaceSchemaRefs
       };
-      const activityValidator = Ajv({
-        allErrors: true,
-        verbose: true,
-        jsonPointers: false,
-        $data: true,
-        schemaId: 'auto',
-        schemas: activitySchemaRefs
-      }).compile(activitySchema);
-      const workplaceValidator = Ajv({
-        allErrors: true,
-        verbose: true,
-        jsonPointers: false,
-        $data: true,
-        schemaId: 'auto',
-        schemas: workplaceSchemaRefs
-      }).compile(workplaceSchema);
 
       let activityEditor = new JSONEditor(activityEditorContainer, activityOptions);
-      activityEditor.validate = activityValidator;
       activityEditorRef.current = activityEditor;
       let workplaceEditor = new JSONEditor(workplaceEditorContainer, workplaceOptions);
-      workplaceEditor.validate = workplaceValidator;
       workplaceEditorRef.current = workplaceEditor;
 
       if (options.appActivity || options.appWorkplace) {
@@ -93,34 +99,50 @@ export default function Validator({ options }) {
     const workplaceEditor = workplaceEditorRef.current;
 
     // Activity
-    let activity = activityEditor.get();
+    let activity = null;
     if (validationType === 'all' || validationType === 'activity') {
-      let validActivity = activityEditor.validate(activity);
-      if (!validActivity) {
-        console.log(activityEditor.validate.errors);
-        setActivityErrors(activityEditor.validate.errors);
-      } else {
-        setActivityErrors([]);
+      try {
+        activity = activityEditor.get();
+      } catch (error) {
+        setActivityErrors([{ keyword: 'syntax', params: error }]);
       }
     } else {
       setActivityErrors(null);
     }
+    if (activity) {    
+      let validateActivity = getActivityValidator();
+      let validActivity = validateActivity(activity);
+      if (!validActivity) {
+        //console.log(validateActivity.errors);
+        setActivityErrors(validateActivity.errors);
+      } else {
+        setActivityErrors([]);
+      }
+    }
 
     // Workplace
-    let workplace = workplaceEditor.get();
+    let workplace = null;
     if (validationType === 'all' || validationType === 'workplace') {
-      let validWorkplace = workplaceEditor.validate(workplace);
-      if (!validWorkplace) {
-        console.log(workplaceEditor.validate.errors);
-        setWorkplaceErrors(workplaceEditor.validate.errors);
-      } else {
-        setWorkplaceErrors([]);
+      try {
+        workplace = workplaceEditor.get();
+      } catch (error) {
+        setWorkplaceErrors([{ keyword: 'syntax', params: error }]);
       }
     } else {
       setWorkplaceErrors(null);
     }
+    if (workplace) {
+      let validateWorkplace = getWorkplaceValidator();
+      let validWorkplace = validateWorkplace(workplace);
+      if (!validWorkplace) {
+        //console.log(validateWorkplace.errors);
+        setWorkplaceErrors(validateWorkplace.errors);
+      } else {
+        setWorkplaceErrors([]);
+      }
+    }
 
-    if (validationType === 'all') {
+    if (activity && workplace && validationType === 'all') {
       // Analyze workplace
       const objectTypes = ['things', 'places', 'persons', 'sensors', 'devices', 'apps',
         'detectables', 'primitives', 'predicates', 'warnings'];
@@ -144,7 +166,7 @@ export default function Validator({ options }) {
         { path: ['actions', 'enter', 'deactivates'], property: 'poi', types: ['things', 'places', 'persons'] } // TODO Verify (only) these are tangible
       ];
       let referenceErrors = [];
-      const checkReferences = (current, path, dataPath, property, types) => {
+      const checkReferences = (current = {}, path, dataPath, property, types) => {
         if (path.length > 0) {
           let segment = path[0];
           let remaining = path.slice(1);
@@ -207,30 +229,111 @@ export default function Validator({ options }) {
         </div>
         <button type="submit" className="btn btn-primary mb-2">Validate</button>
       </form>
-      {activityErrors === null ? <></> :
+      {!activityErrors ? <></> :
         <div className="my-2">
-          <h3>Activity</h3>
-          {activityErrors.map((activityError, index) => (
-            <div key={index}>{activityError.dataPath}: {activityError.keyword} ({JSON.stringify(activityError.params)})</div>
-          ))}
+          <h3><StatusIcon count={activityErrors.length} /> Activity: {activityErrors.length} error{activityErrors.length !== 1 ? 's' : ''}</h3>
+          <ol>
+            {activityErrors.map((activityError, index) => (
+              <ValidationError key={index} error={activityError} modelName="Activity" />
+            ))}
+          </ol>
         </div>
       }
-      {workplaceErrors === null ? <></> :
+      {!workplaceErrors ? <></> :
         <div className="my-2">
-          <h3>Workplace</h3>
-          {workplaceErrors.map((workplaceError, index) => (
-            <div key={index}>{workplaceError.dataPath}: {workplaceError.keyword} ({JSON.stringify(workplaceError.params)})</div>
-          ))}
+          <h3><StatusIcon count={workplaceErrors.length} /> Workplace: {workplaceErrors.length} error{workplaceErrors.length !== 1 ? 's' : ''}</h3>
+          <ol>
+            {workplaceErrors.map((workplaceError, index) => (
+              <ValidationError key={index} error={workplaceError} modelName="Workplace" />
+            ))}
+          </ol>
         </div>
       }
-      {referenceErrors === null ? <></> :
+      {!referenceErrors ? <></> :
         <div className="my-2">
-          <h3>References</h3>
-          {referenceErrors.map((referenceError, index) => (
-            <div key={index}>{referenceError.dataPath}: {referenceError.keyword} ({JSON.stringify(referenceError.params)})</div>
-          ))}
+          <h3><StatusIcon count={referenceErrors.length} /> References: {referenceErrors.length} error{referenceErrors.length !== 1 ? 's' : ''}</h3>
+          <ol>
+            {referenceErrors.map((referenceError, index) => (
+              <ValidationError key={index} error={referenceError} />
+            ))}
+          </ol>
         </div>
       }
     </div>
+  ); sr
+}
+
+function ValidationError({ error, modelName = '' }) {
+  let details, { params } = error;
+  switch (error.keyword) {
+    case 'syntax':
+      details = <>JSON syntax error: <pre>{params.message}</pre></>;
+      break;
+    case 'required':
+      details = <>Missing property, must be specified: <code>{params.missingProperty}</code></>;
+      break;
+    case 'additionalProperties':
+      details = <>Non-standard property, shouldn't be specified: <code>{params.additionalProperty}</code></>;
+      break;
+    case 'enum':
+      details = <>Invalid value, must be one of: {params.allowedValues.map((value, index) => (<>
+        {index === 0 ? <></> : <>, </> }
+        <code key={index}>{value}</code>
+      </>))}</>;
+      break;
+    case 'format':
+      details = <>Invalid value, must be formatted as: <code>{params.format}</code></>;
+      break;
+    case 'notFound':
+      details = <>Reference <code>{params.objectId}</code> not found for property <code>{params.property}</code>,
+        must be a reference to one of: {params.types.map((type, index) => (<>
+        {index === 0 ? <></> : <>, </> }
+        <code key={index}>{type}</code>
+      </>))}</>;
+      break;
+    default:
+      details = <>{error.keyword} ({JSON.stringify(error.params)});</>;
+  }
+  return (
+    <li><em>{error.dataPath || modelName}:</em> {details}</li>
   );
+}
+
+function CircleFill() {
+  return (
+    // Source: https://icons.getbootstrap.com/icons/check-circle-fill/ (MIT license)
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+    </svg>
+  );
+}
+
+function CheckCircleFill() {
+  return (
+    // Source: https://icons.getbootstrap.com/icons/check-circle-fill/ (MIT license)
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+    </svg>
+  );
+}
+
+function XCircleFill() {
+  return (
+    // Source: https://icons.getbootstrap.com/icons/x-circle-fill/ (MIT license)
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+    </svg>
+  );
+}
+
+function StatusIcon({ count }) {
+ if (count === 0) {
+   return (
+    <span style={{ color: 'green' }}><CheckCircleFill /></span>
+   );
+ } else {
+   return (
+    <span style={{ color: 'red' }}><XCircleFill /></span>
+   );
+ }
 }
